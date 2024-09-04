a2275ac338e045617eb444a20890059f5f2dda46
arefdeveloper91 <149487876+arefdeveloper91@users.noreply.github.com>
2024-09-04 12:58:38 -0300

    Initial Commit

---
+++ loginPratices/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loginPratices
+//
+//  Created by Aref Chucri on 04/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ loginPratices/loginPraticesApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginPraticesApp.swift
+//  loginPratices
+//
+//  Created by Aref Chucri on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct loginPraticesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
